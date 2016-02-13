f27974aca8ee9a9fb2cfa3336b2be49a722babfb
Isaiah Fentress <perrotti@onid.oregonstate.edu>
2016-02-13 12:39:47 -0800

    Changed the .js file to remove my API key

--- AJAX_Assignment/weather.js
+++ AJAX_Assignment/weather.js
@@ -1,4 +1,4 @@
-var key = "b65bd77d0451298f757069747d71a4de"; // User can input their openweathermap API key here
+var key = ""; // User can input their openweathermap API key here
 
 // This section of the javascript creates the output elements (<p>) for the results of city, temp, and humidity
 var outputDiv = document.getElementById("output-div");
