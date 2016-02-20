1acc4f228eac152a5b0465e58e3b69f636b9efb6
Ubuntu <ubuntu@ip-172-31-21-30.us-west-2.compute.internal>
2016-02-20 19:25:30 +0000

    Updated the hello.js file

--- GETPOST_Assignment/hello.js
+++ GETPOST_Assignment/hello.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function(req,res){
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world from Isaiah!');
-}).listen(2000);
+}).listen(5000);
 
-console.log('Server started on localhost:2000; press Ctrl-C to terminate....');
+console.log('Server started on localhost:5000; press Ctrl-C to terminate....');
