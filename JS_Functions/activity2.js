b9c675fce2f3c7e825d04164388246b569fbf147
Ubuntu <ubuntu@ip-172-31-21-30.us-west-2.compute.internal>
2016-02-20 19:43:02 +0000

    Updating to correct port

--- JS_Functions/activity2.js
+++ JS_Functions/activity2.js
@@ -6,8 +6,8 @@ function concat1(x, y) {
 	return x + y;
 };
 
-// This block of code will break unless concat2 is changed to concat 1.
-// This is becuase var decleration of concat2 does not take place until after it is called
+// This block of code will break unless concat2 is changed to concat1.
+// This is becuuase var declaration of concat2 does not take place until after it is called
 // To test the block of code above, change concat2 to concat1 so the program doesn't break
 console.log("Calling concat2 function now:");
 result = concat2("Banana ", "split");
