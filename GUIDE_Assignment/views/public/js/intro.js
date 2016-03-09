4c86d32411a9ba0306e4724b854cab454cf3c430
Isaiah Fentress <perrotti@onid.oregonstate.edu>
2016-03-09 04:43:55 -0800

    More prettify stuff

--- GUIDE_Assignment/views/public/js/intro.js
+++ GUIDE_Assignment/views/public/js/intro.js
@@ -15,8 +15,8 @@ function drawChart() {
 
   // Set chart options
   var options = {title:'Pick Up Line Success Rate (%)',
-                 width:400,
-                 height:400,
+                 width:800,
+                 height:600,
                  backgroundColor: {strokeWidth:3},
                  cssClassNames: {headerCell: 'googleHeaderCell'}};
 
