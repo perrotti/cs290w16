26ec319533ea42e606daef72d5b2be9e94392a82
Fentress <fentress@amazon.com>
2016-03-09 14:28:59 -0800

    Added API test items

--- GUIDE_Assignment/views/public/js/apitest.js
+++ GUIDE_Assignment/views/public/js/apitest.js
@@ -6,18 +6,18 @@ function drawChart() {
 
   // Create the data you want to include in your chart
   var data = new google.visualization.arrayToDataTable([
-    [dataTitle, 'Price'],
-    ['Low', dataLow],
-    ['Current Price', dataPrice],
-    ['High', dataHigh]
+    [dataOne, 'Price'],
+    ['Low', dataTwo],
+    ['Current Price', dataThree],
+    ['High', dataFour]
   ]);
 
-  var options = {title:'Places I Have Eaten Lunch This Month',
+  var options = {title:dataOne + 'Places Stock Price',
                  width:450,   // Width of chart
                  height:450,  // Height of chart
                  backgroundColor: {strokeWidth:3}}  // Put a border around the chart
 
-  var chart = new google.visualization.PieChart(document.getElementById('api_test_chart'));
+  var chart = new google.visualization.BarChart(document.getElementById('api_test_chart'));
   
   chart.draw(data, options);
 }
