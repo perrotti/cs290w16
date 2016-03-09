9f5ec69e9ccd5095febc7daa1fba2d2b2df1847a
Fentress <fentress@amazon.com>
2016-03-09 11:02:07 -0800

    Added files for website

--- GUIDE_Assignment/views/public/js/basicsyntax.js
+++ GUIDE_Assignment/views/public/js/basicsyntax.js
@@ -25,8 +25,8 @@ function drawChart() {
 
   // Set options for how the chart will be displayed
   var options = {title:'Places I Have Eaten Lunch This Month',
-                 width:400px,   // Width of chart
-                 height:400px,  // Height of chart
+                 width:400,   // Width of chart
+                 height:400,  // Height of chart
                  backgroundColor: {strokeWidth:3}}  // Put a border around the chart
 
   /* This line does several things:
