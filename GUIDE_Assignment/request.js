1eb94ec5176e763a842c2c74191342db2482b244
Fentress <fentress@amazon.com>
2016-03-09 13:13:46 -0800

    Added API test items

--- GUIDE_Assignment/request.js
+++ GUIDE_Assignment/request.js
@@ -38,18 +38,24 @@ app.get('/apitest', function(req, res, next) {
   request('http://finance.yahoo.com/webservice/v1/symbols/AAPL/quote?format=json&view=detail', function(err, response, body) {
     if(!err && response.statusCode < 400) {
       var info = JSON.parse(body);
-      input.one = info;
-      input.two = info.list;
-      input.three = info.list.resources;
-      input.four = info.list.resources[0].resource;
-      input.five = info.list.resources[0].resource.fields.name;
+      
+      input.one = info.list.resources[0].resource.fields.name;
+      
       var temp = info.list.resources[0].resource.fields.year_low;
-      input.six = Number(temp).toFixed(2);
+      input.two = Number(temp).toFixed(2);
+      
       temp = info.list.resources[0].resource.fields.day_low;
-      input.seven = Number(temp).toFixed(2);
-      input.eight = info.list.resources[0].resource.fields.price;
-      input.nine = info.list.resources[0].resource.fields.day_high;
-      input.ten = info.list.resources[0].resource.fields.year_high;
+      input.three = Number(temp).toFixed(2);
+      
+      temp = info.list.resources[0].resource.fields.price;
+      input.four = Number(temp).toFixed(2);
+      
+      temp = info.list.resources[0].resource.fields.day_high;
+      input.five = Number(temp).toFixed(2);
+      
+      temp = info.list.resources[0].resource.fields.year_high;
+      input.six = Number(temp).toFixed(2);
+      
       res.render('apitest', input);
     } else {
       if(response) {
@@ -74,3 +80,21 @@ app.use(function(err, req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.')
 });
+
+
+/*Code to parse YAHOO API INFORMATION
+Saved for reference
+
+var info = JSON.parse(body);
+input.one = info;
+input.two = info.list;
+input.three = info.list.resources;
+input.four = info.list.resources[0].resource;
+input.five = info.list.resources[0].resource.fields.name;
+var temp = info.list.resources[0].resource.fields.year_low;
+input.six = Number(temp).toFixed(2);
+temp = info.list.resources[0].resource.fields.day_low;
+input.seven = Number(temp).toFixed(2);
+input.eight = info.list.resources[0].resource.fields.price;
+input.nine = info.list.resources[0].resource.fields.day_high;
+input.ten = info.list.resources[0].resource.fields.year_high;*/
