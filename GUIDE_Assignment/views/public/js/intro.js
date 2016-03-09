9e4f9a1a18b44ffa7d2001351d09e77208a97ac4
Isaiah Fentress <perrotti@onid.oregonstate.edu>
2016-03-09 02:44:35 -0800

    Added intro page content

---
+++ GUIDE_Assignment/views/public/js/intro.js
@@ -0,0 +1,23 @@
+// Function creates data, chart options, and draws chart
+function drawChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Line');
+  data.addColumn('Success', 'Percentage');
+  data.addRows([
+    ['Girl, you better have a license, cuz you are driving me crazy', 5],
+    ['If beauty were measured in seconds, you would be an hour!', 7],
+    ['Are you Sweadish? cuz you are the sweetish girl I have met!', 2],
+  ]);
+
+  // Set chart options
+  var options = {title:'Pick Up Line Success Rate (%)',
+                 width:600,
+                 height:500,
+                 colors: ['#990000', '#ff3333', '#ff9999']};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.BarChart(document.getElementById('pick-up'));
+  chart.draw(data, options);
+}
