25f5f448dd72d3fd5d1359ac167779fa4ac33e9d
Isaiah Fentress <perrotti@onid.oregonstate.edu>
2016-03-09 03:18:52 -0800

    changing to colum

--- GUIDE_Assignment/views/public/js/intro.js
+++ GUIDE_Assignment/views/public/js/intro.js
@@ -6,25 +6,21 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 
   // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Line');
-  data.addColumn('number', 'Percentage');
-  data.addRows([
-    ['Girl, you better have a license, cuz you are driving me crazy', 5],
-    ['If beauty were measured in seconds, you would be an hour!', 7],
-    ['Are you Sweadish? cuz you are the sweetish girl I have met!', 2],
+  var data = new google.visualization.arrayToDataTable([
+    ['Line', 'Percentage', {role: 'style'}],
+    ['Girl, you better have a license, cuz you are driving me crazy', 5, '#990000'],
+    ['If beauty were measured in seconds, you would be an hour!', 7, '#ff3333'],
+    ['Are you Sweadish? cuz you are the sweetish girl I have met!', 2, '#ff9999']
   ]);
 
   // Set chart options
   var options = {title:'Pick Up Line Success Rate (%)',
                  width:800,
                  height:500,
-                 bars: 'vertical',
                  legend: {position: 'top'},
-                 cssClassNames: {headerCell: 'googleHeaderCell'},
-                 colors: ['#990000', '#ff3333', '#ff9999']};
+                 cssClassNames: {headerCell: 'googleHeaderCell'}};
 
   // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.BarChart(document.getElementById('pick-up'));
+  var chart = new google.visualization.ColumnChart(document.getElementById('pick-up'));
   chart.draw(data, options);
 }
